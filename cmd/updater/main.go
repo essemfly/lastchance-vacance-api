9f1ce6b02ef85528bfa2464529f2bd853f29e4c8
seokmin <essemfly@gmail.com>
2023-03-04 17:54:42 +0900

    Make updater work

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/1000king/handover/cmd"
 	"github.com/1000king/handover/config"
 	"github.com/1000king/handover/internal/domain"
+	"github.com/1000king/handover/pkg/crawler"
 	"github.com/1000king/handover/pkg/product"
 	"go.uber.org/zap"
 )
@@ -17,13 +19,19 @@ func main() {
 		crawlProductFilter := &domain.CrawlProductFilter{
 			Status: domain.DANGGN_STATUS_SALE,
 		}
-		offset, limit := 0, 10000
+		offset, limit := 0, 1000
 		crawlPds, total, err := config.Repo.CrawlProducts.List(crawlProductFilter, offset, limit)
 		if err != nil {
 			config.Logger.Error("failed to list product", zap.Error(err))
 		}
 		for _, pd := range crawlPds {
-			updateCrawledProductStatus(pd)
+			danggnIndex, _ := strconv.Atoi(pd.DanggnIndex)
+			updatedCrawlProduct, err := crawler.CrawlPage(danggnIndex)
+			if err != nil {
+				zap.Error(err)
+				continue
+			}
+			updateCrawledProduct(pd, updatedCrawlProduct)
 			product.AddProductInCrawled(pd)
 		}
 
@@ -35,7 +43,13 @@ func main() {
 					config.Logger.Error("failed to list product", zap.Error(err))
 				}
 				for _, pd := range crawlPds {
-					updateCrawledProductStatus(pd)
+					danggnIndex, _ := strconv.Atoi(pd.DanggnIndex)
+					updatedCrawlProduct, err := crawler.CrawlPage(danggnIndex)
+					if err != nil {
+						zap.Error(err)
+						continue
+					}
+					updateCrawledProduct(pd, updatedCrawlProduct)
 					product.AddProductInCrawled(pd)
 				}
 			}
@@ -44,7 +58,14 @@ func main() {
 	}
 }
 
-func updateCrawledProductStatus(pd *domain.CrawlProduct) {
+func updateCrawledProduct(pd *domain.CrawlProduct, newPd *domain.CrawlProduct) {
+	pd.Status = newPd.Status
+	pd.ViewCounts = newPd.ViewCounts
+	pd.ChatCounts = newPd.ChatCounts
+	pd.LikeCounts = newPd.LikeCounts
+	pd.Price = newPd.Price
+	pd.UpdatedAt = time.Now()
+
 	_, err := config.Repo.CrawlProducts.Update(pd)
 	if err != nil {
 		config.Logger.Error("failed to update product", zap.Error(err))
