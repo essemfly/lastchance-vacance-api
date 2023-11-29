84b83afa139d6ee7e1506c022bd4f406ed2cff3f
Seokmin Lee <essemfly@gmail.com>
2023-11-29 10:20:56 +0900

    Hide slack api

--- config/slack.go
+++ config/slack.go
@@ -4,13 +4,14 @@ import (
 	"fmt"
 
 	"github.com/slack-go/slack"
+	"github.com/spf13/viper"
 )
 
 var slackClient *slack.Client
 
 func InitSlack() {
 	// Create a new Slack client with your API token
-	slackClient = slack.New("xoxb-4368315466963-5185879956006-awiabYQEMGBonfUcsI6VdUMH")
+	slackClient = slack.New(viper.GetString("SLACK_API_TOKEN"))
 }
 
 func SendSlackMessage(message string) {
